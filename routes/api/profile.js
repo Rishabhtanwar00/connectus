fd1a0ba2393cb220e8205de29a6211907f39ae82
Rishabh Tanwar <rishabhtanwar2000@gmail.com>
2022-11-21 23:58:08 +0530

    added profile model and created get current user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,27 @@
 import express from 'express';
+import auth from '../../middleware/auth.js';
+import Profile from '../../models/Profile.js';
+import User from '../../models/User.js';
 const router = express.Router();
 
-// @route   GET api/profile
-// @desc    Test Route
-// @access  public
-router.get('/', (req, res) => {
-	res.send('Profile route');
+// @route   GET api/profile/me
+// @desc    get current users profile
+// @access  private
+router.get('/me', auth, async (req, res) => {
+	try {
+		const profile = await Profile.findOne({ user: req.user.id }).populate(
+			'user',
+			['name', 'avatar']
+		);
+
+		if (!profile) {
+			return res.status(400).json({ msg: 'No profile found for this user' });
+		}
+		res.json(profile);
+	} catch (err) {
+		console.error(err.message);
+		return res.status(500).send('server error');
+	}
 });
 
 export default router;
