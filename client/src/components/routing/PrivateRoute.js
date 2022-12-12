b78bb605a6976c9e27e09449eff7ec33b26db5c7
Rishabh Tanwar <rishabhtanwar2000@gmail.com>
2022-12-12 18:25:32 +0530

    added privateRoute and create-profile component

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Outlet, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ auth: { isAuthenticated, loading }, children }) =>
 	!isAuthenticated && !loading ? <Navigate to='/login' /> : children;
