7f68baa7bc7437586886ced69342467003a8ca18
Rishabh Tanwar <rishabhtanwar2000@gmail.com>
2022-12-28 23:32:04 +0530

    completed profile and dashboard

---
+++ client/src/components/routing/withRouter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useNavigate } from 'react-router';
+const withRouter = (Component) => {
+	const Wrapper = (props) => {
+		const history = useNavigate();
+		return <Component history={history} {...props} />;
+	};
+	return Wrapper;
+};
+
+export default withRouter;
