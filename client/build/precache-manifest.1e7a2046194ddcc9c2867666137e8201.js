e045444b76d55ab1e106b17756d982475f95f8c8
Rishabh Tanwar <rishabhtanwar2000@gmail.com>
2024-01-24 18:53:01 +0530

    ..

---
+++ client/build/precache-manifest.1e7a2046194ddcc9c2867666137e8201.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b9c1b1663ef3bf4d2de",
+    "url": "/static/css/main.b3b4b36d.chunk.css"
+  },
+  {
+    "revision": "7b9c1b1663ef3bf4d2de",
+    "url": "/static/js/main.7b9c1b16.chunk.js"
+  },
+  {
+    "revision": "b714582c903195de2f31",
+    "url": "/static/js/1.b714582c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b0d7feaea8136a13f6bfacfebc14ca26",
+    "url": "/static/media/Spinner.b0d7feae.gif"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  },
+  {
+    "revision": "5a0ff60e83d789431a726a34b56db347",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -23,7 +23,8 @@ app.use('/api/posts', postsRoute);
 //serve static asset in production
 if (process.env.NODE_ENV === 'production') {
 	//set static folder
-	app.use(express.static('client/build'));
+	const __dirname = path.resolve();
+	app.use(express.static(path.join(__dirname, 'client/build')));
 
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
